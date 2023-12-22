32bfbeeb8e75c6bca3e487999235b14dd381fb35
Nikolai <kolamba.r@gmail.com>
2023-12-22 16:24:27 +0300

    init

---
+++ manufacturers_and_retailers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManufacturersAndRetailersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manufacturers_and_retailers'
---
+++ manufacturers_and_retailers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+
+app_name = 'manufacturers_and_retailers'
+
+urlpatterns = [
+
+]
